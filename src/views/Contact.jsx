dfb4c2374d6e4410c9296da5f849618feec3fa9b
Stefan <blockchain.crypto.stefan@gmail.com>
2023-09-17 12:29:04 +0100

    Added Contact icons

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,6 +1,22 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
+import { AtSign, Linkedin, Github, FileText } from "lucide-react";
+import Container from "../components/ui/Container";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <Container width="w-full">
+      <Container className="text-center">
+        <h1 className="text-6xl text-center my-5">Contact</h1>
+        <div>Form goes here</div>
+        <div className="flex text-center justify-center mt-5">
+          <AtSign />
+          <Linkedin />
+          <Github />
+          <FileText />
+        </div>
+      </Container>
+    </Container>
+  );
 }
